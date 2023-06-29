b23a6c308b9b55bd822628b59c4248ff844aa985
Andy Cockroft <acockroft1994@gmail.com>
2023-06-29 15:39:17 +0200

    frontend examples, graph and country display

---
+++ rest-countries/index.js
@@ -0,0 +1,28 @@
+// index.js
+
+const getCountryInfo = (countryName) => {
+  axios
+    .get(`https://restcountries.com/v2/name/${countryName}`)
+    .then((response) => {
+      const countryDetail = response.data[0];
+      document.getElementById("country-name").innerText = countryDetail.name;
+      document.getElementById("country-capital").innerText =
+        countryDetail.capital;
+      document
+        .getElementById("country-flag")
+        .setAttribute("src", countryDetail.flag);
+      document.getElementById("currency").innerHTML =
+        countryDetail.currencies[0].code;
+    })
+    .catch((err) => {
+      console.log(err);
+      err.response.status === 404
+        ? alert(`The country ${countryName} doesn't exist.`)
+        : alert("Server error! Sorry.");
+    });
+};
+
+document.getElementById("get-country-btn").addEventListener("click", () => {
+  const userInput = document.getElementById("country-name-input").value;
+  getCountryInfo(userInput);
+});
